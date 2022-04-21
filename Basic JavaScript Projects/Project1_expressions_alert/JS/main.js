99ae43d384f612f7c893b280060ddbee47ed14b2
altrdstates <100340019+altrdstates@users.noreply.github.com>
2022-04-21 13:48:59 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
    linking html to JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+var X= "Here's a pop up window!" //assigning a variable
+window.alert(X);
+
+document.write(X.fontcolor("red"));
+
+var A= "and here is a special message," //assigning a variable
+document.write(A);
+
+var Y= "F"
+var Z= "G"
+var C= Y+Z //concatenating the two variables
+document.write(C)
+
+function My_First_Function() {  //Defining a function and naming it
+    var str = "This is the button text!"; //Defiing a variable and giving it a string value
+    document.getElementById("Button_Text") .innerHTML = str; //Putting the value of the variable into the HTML elementFROMPOINT with the "Button_Text" id
+}
